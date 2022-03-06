aad29f10176e779bf7762c6f2c69ba0fb99ad668
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-03-06 17:28:17 +0100

    Add command line arguments

--- Cryptography/Algorithms/Program.cs
+++ Cryptography/Algorithms/Program.cs
@@ -1,6 +1,24 @@
 ﻿using Algorithms;
 
 var playfair = new Playfair(new DuplicateRemover());
+
+/*
 playfair.GenerateKeyTable("playfair example");
-Console.WriteLine(playfair.Encipher("hide tee in the spot"));
-Console.WriteLine(playfair.Decipher("BMODVIXRKUDMKFKV"));
+Console.WriteLine(playfair.Encipher("hide the gold in the tree stump"));
+Console.WriteLine(playfair.Decipher("BMODZBXDNABEKUDMUIXMMOUVIF"));
+*/
+
+var type = args[0].ToLower();
+var key = args[1];
+var text = args[2];
+
+playfair.GenerateKeyTable(key);
+
+if(type == "encrypt")
+{
+    Console.WriteLine(playfair.Encipher(text.ToLower()));
+}
+else if(type == "decrypt")
+{
+    Console.WriteLine(playfair.Decipher(text.ToUpper()));
+}
