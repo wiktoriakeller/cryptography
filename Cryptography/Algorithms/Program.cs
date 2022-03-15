f77e04175bc13c1b88d31dc973d1c3720f878c9f
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-03-15 21:47:14 +0100

    Update command line properties

--- Cryptography/Algorithms/Program.cs
+++ Cryptography/Algorithms/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Algorithms.Playfair;
 
-var key = "playfair";
-var plainText = "Like most classical ciphers, the Playfair cipher can be easily cracked if there is enough text lol.";
-
+/*
+var key = "playfair example";
+var plainText = "Like most classical ciphers, the Playfair cipher can be easily cracked if there is enough text.";
 var playfair = new Playfair(new KeyTable());
 playfair.LeaveOnlyLetters(false);
 playfair.GenerateKeyTable(key);
@@ -14,6 +14,7 @@ Console.WriteLine($"Key: {key}");
 Console.WriteLine($"Plain text: {plainText}");
 Console.WriteLine();
 Console.WriteLine($"Encrypted text: {enciphered}");
+Console.WriteLine();
 Console.WriteLine($"Decrypted text: {deciphered}");
 Console.WriteLine();
 
@@ -23,21 +24,34 @@ var enciphered2 = playfairExtended.Encipher(plainText);
 var deciphered2 = playfairExtended.Decipher(enciphered2);
 
 Console.WriteLine($"Encrypted text 2: {enciphered2}");
+Console.WriteLine();
 Console.WriteLine($"Decrypted text 2: {deciphered2}");
+*/
 
-/*
 var type = args[0].ToLower();
-var key = args[1];
-var text = string.Join(" ", args[2..]);
+var method = args[1].ToLower();
+var key = args[2];
+var text = string.Join(" ", args[3..]);
+PlayfairBase playfair;
+
+if(type == "extended")
+{
+    playfair = new PlayfairExtended(new KeyTableExtended());
+}
+else
+{
+    playfair = new Playfair(new KeyTable());
+    var normalPlayfair = playfair as Playfair;
+    normalPlayfair?.LeaveOnlyLetters(false);
+}
 
 playfair.GenerateKeyTable(key);
 
-if (type == "encrypt")
+if (method == "encrypt")
 {
     Console.WriteLine($"Encrypted text: {playfair.Encipher(text)}");
 }
-else if (type == "decrypt")
+else
 {
     Console.WriteLine($"Decrypted text: {playfair.Decipher(text)}");
 }
-*/
